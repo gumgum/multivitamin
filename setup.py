505ad5caa17266c3bafbd684c27c0982317ec627
GregChu <gchu@gumgum.com>
2019-01-11 14:23:23 -0800

    ver up

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import sys
 from setuptools import setup, find_packages
 from setuptools.command.install import install
 
-VERSION = '1.1.1'
+VERSION = '1.1.2'
 
 reqs = None
 with open('requirements.txt') as rf:
