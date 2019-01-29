585d4e0e793212e17847821876c7cee269ed8bf8
Matthew Greenberg <mgreenberg@gumgum.com>
2019-01-29 13:48:59 -0800

    import error

--- tests/test_ssd_detector.py
+++ tests/test_ssd_detector.py
@@ -51,6 +51,8 @@ import json
 import os
 from io import BytesIO
 
+from utils import generate_fileobj_from_s3_folder
+
 from vitamincv.applications.detectors.ssd_detector import SSDDetector
 from vitamincv.module_api.utils import load_idmap
 
