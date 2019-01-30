7e9ac371ff7c575bb602ce2329f183e5dcc4e62b
Javier Molina <javier@gumgum.com>
2019-01-30 10:26:20 -0800

    Allowing double type for xs and ys

--- vitamincv/avro_api/cv_schema_factory.py
+++ vitamincv/avro_api/cv_schema_factory.py
@@ -90,12 +90,12 @@ def create_point(x=0.0, y=0.0, bound=False, ub_x=1.0, ub_y=1.0):
     Returns:
         dict: x,y 
     """
-    if not isinstance(x, float):
+    if not isinstance(x, float) and not isinstance(x, double):
         log.warning("x should be a float")
-        x = float(x)
-    if not isinstance(y, float):
+    x = float(x)
+    if not isinstance(y, float) and not isinstance(x, double):
         log.warning("y should be a float")
-        y = float(y)
+    y = float(y)
     lowerbound = 0.0
     if bound:
         x = min(max(lowerbound, x), ub_x)
