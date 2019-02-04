5cb1fa95fe2e3313feb8a08592fd049d9bb440a0
Matthew Greenberg <mgreenberg@gumgum.com>
2019-02-04 11:19:19 -0800

    adding local dump

--- vitamincv/applications/general/frame_extractor.py
+++ vitamincv/applications/general/frame_extractor.py
@@ -9,14 +9,17 @@ from io import BytesIO
 from imohash import hashfileobject
 import boto3
 
+import os
+
 class FrameExtractor(CVModule):
-    def __init__(self, server_name, version, sample_rate=1.0):
+    def __init__(self, server_name, version, sample_rate=1.0, s3_bucket=None, local_dir=None):
         super().__init__(server_name, version)
         self._sample_rate = sample_rate
 
-        self._s3_bucket = "unified-frame-extraction"
+        self._local_dir = local_dir
+        self._s3_bucket = s3_bucket
         self._list_file = "contents"
-        self._s3_key_format = "{video_hash}/{filename}.{ext}"
+        self._rel_path_format = "{video_hash}/{filename}.{ext}"
         self._s3_url_format = "https://s3.amazonaws.com/{bucket}/{s3_key}"
 
         self._sql_db = ""
@@ -25,20 +28,56 @@ class FrameExtractor(CVModule):
 
         self._encoding = "JPEG"
 
-        self._manager = WorkerManager(func=self._upload_frame_helper,
+        self._s3_write_manager = WorkerManager(func=self._upload_frame_helper,
                                       n=100,
                                       max_queue_size=100,
                                       parallelization="thread")
+
+        self._local_write_manager = WorkerManager(func=self._write_frame_helper,
+                                      n=100,
+                                      max_queue_size=100,
+                                      parallelization="thread")
+
+    @staticmethod
+    def _mklocaldirs(directory):
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+    def _write_frame_helper(self, data):
+        self._write_frame(**data)
+
+    def _write_frame(self, frame, tstamp, video_hash):
+        relative_path = self._rel_path_format.format(video_hash=video_hash, filename=tstamp, ext=self._encoding)
+        full_path = "{}/{}".format(self._local_dir, relative_path)
+        im_filelike = self._convert_frame_to_filelike(frame)
+        with open(full_path, "wb") as f:
+            f.write(im_filelike.read())
+
+    def _add_contents_to_local(self, contents):
+        filelike = BytesIO()
+        for video_hash, tstamp in contents:
+            relative_path = self._rel_path_format.format(video_hash=video_hash, filename=tstamp, ext=self._encoding)
+            full_path = "{}/{}".format(self._local_dir, relative_path)
+            line = "{}\t{}\n".format(tstamp, full_path)
+            filelike.write(line.encode())
+        filelike.seek(0)
+
+        with open("{}/{}".format(self._local_dir, self.contents_file_key), "wb") as f:
+            f.write(filelike.read())
+
+    def _convert_frame_to_filelike(self, frame):
+        im = Image.fromarray(frame)
+        im_filelike = BytesIO()
+        im.save(im_filelike, format=self._encoding)
+        im_filelike.seek(0)
+        return im_filelike
 
     def _upload_frame_helper(self, data):
         self._upload_frame(**data)
 
     def _upload_frame(self, frame, tstamp, video_hash):
-        im = Image.fromarray(frame)
-        im_filelike = BytesIO()
-        im.save(im_filelike, format=self._encoding)
-        im_filelike.seek(0)
-        s3_key = self._s3_key_format.format(video_hash=video_hash, filename=tstamp, ext=self._encoding)
+        im_filelike = self._convert_frame_to_filelike(frame)
+        s3_key = self._rel_path_format.format(video_hash=video_hash, filename=tstamp, ext=self._encoding)
         result = self._s3_client.upload_fileobj(im_filelike,
                                                 self._s3_bucket,
                                                 s3_key)
@@ -50,7 +89,7 @@ class FrameExtractor(CVModule):
     def _add_contents_to_s3(self, contents):
         filelike = BytesIO()
         for video_hash, tstamp in contents:
-            s3_key = self._s3_key_format.format(video_hash=video_hash, filename=tstamp, ext=self._encoding)
+            s3_key = self._rel_path_format.format(video_hash=video_hash, filename=tstamp, ext=self._encoding)
             im_url = self._s3_url_format.format(bucket=self._s3_bucket, s3_key=s3_key)
             line = "{}\t{}\n".format(tstamp, im_url)
             filelike.write(line.encode())
@@ -72,10 +111,16 @@ class FrameExtractor(CVModule):
             self.code = "ERROR_NO_IMAGES_LOADED"
 
         video_hash = hashfileobject(filelike, hexdigest=True)
-        self.contents_file_key = self._s3_key_format.format(video_hash=video_hash, filename=self._list_file, ext="tsv")
+        self.contents_file_key = self._rel_path_format.format(video_hash=video_hash, filename=self._list_file, ext="tsv")
+
+        if self._local_dir is not None:
+            self._mklocaldirs("{}/{}".format(self._local_dir, video_hash))
+            if os.path.exists("{}/{}".format(self._local_dir, self.contents_file_key)):
+                log.info("Local Video already exists")
+
         try:
             self._s3_client.head_object(Bucket=self._s3_bucket,
-                                    Key=self._s3_key_format.format(video_hash=video_hash,
+                                    Key=self._rel_path_format.format(video_hash=video_hash,
                                                                    filename=self._list_file,
                                                                    ext="tsv"))
             log.info("Video already exists")
@@ -95,9 +140,17 @@ class FrameExtractor(CVModule):
                 "video_hash": video_hash
             }
             contents.append((video_hash, tstamp))
-            self._manager.queue.put(data)
-        self._manager.kill_workers_on_completion()
+
+            if self._local_dir is not None:
+                self._local_write_manager.queue.put(data)
+
+            if self._s3_bucket is not None:
+                self._s3_write_manager.queue.put(data)
+
+        self._s3_write_manager.kill_workers_on_completion()
+        self._local_write_manager.kill_workers_on_completion()
         result = self._add_contents_to_s3(contents)
+        self._add_contents_to_local(contents)
 
     def update_response(self):
         date = get_current_time()
