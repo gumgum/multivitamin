f1df3c766b0df27ae00753b8b0a2f0f4566e93b6
GregChu <gchu@gumgum.com>
2019-02-28 11:44:08 -0800

    axe to tools

--- tools/visualize_frame_anns.py
+++ tools/visualize_frame_anns.py
@@ -4,16 +4,19 @@ import argparse
 
 import glog as log
 
-from vitamincv.avro_api.frame_drawer import FrameDrawer
+from vitamincv.data.response.frame_drawer import FrameDrawer
 
 
 if __name__ == "__main__":
     a = argparse.ArgumentParser("python3 visualize_detections --avro test.avro")
     a.add_argument("--avro", type=str, help="path to avro json doc")
     a.add_argument("--decode", action="store_true", help="if avro is binary")
-    a.add_argument("--dump_to_folder", action="store_true", help="write images to file")
-    a.add_argument("--dump_dir", default="./tmp")
+    a.add_argument("--dump_dir")
     args = a.parse_args()
 
-    fd = FrameDrawer(args.avro, None, args.decode, args.dump_to_folder, args.dump_dir)
+    dump_to_folder = False
+    if args.dump_dir:
+        dump_to_folder = True
+
+    fd = FrameDrawer(args.avro, None, args.decode, dump_to_folder, args.dump_dir)
     fd.process()
