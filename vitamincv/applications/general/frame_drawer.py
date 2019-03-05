d012ded483fb44984b89a9ecfa20d5e27fd0c2fc
Javier Molina <javier@gumgum.com>
2019-03-05 11:34:06 -0800

    First version working as a module

--- vitamincv/applications/general/frame_drawer.py
+++ vitamincv/applications/general/frame_drawer.py
@@ -13,6 +13,8 @@ from vitamincv.module_api.cvmodule import CVModule
 from vitamincv.avro_api.avro_api import AvroIO, AvroAPI
 from vitamincv.avro_api.utils import p0p1_from_bbox_contour
 from vitamincv.media_api.media import MediaRetriever
+from vitamincv.avro_api.utils import get_current_time
+from vitamincv.avro_api.cv_schema_factory import *
 import cv2
 
 COLORS = ['darkorchid', 'darkgreen', 'coral', 'darkseagreen', 
@@ -31,9 +33,10 @@ def get_props_from_region(region):
     return prop_strs
 
 DEFAULT_DUMP_FOLDER='/tmp'
+
 class FrameDrawer(CVModule):
-    def __init__(self, avro_api=None,med_ret=None,module_id_map=None,pushing_folder=DEFAULT_DUMP_FOLDER, s3_bucket=None, s3_key=None):
-        super().__init__(server_name='FrameDrawer', version='1.0.0', module_id_map=module_id_map)
+    def __init__(self, avro_api=None,med_ret=None, server_name='FrameDrawer', version='1.0.0', module_id_map=None,pushing_folder=DEFAULT_DUMP_FOLDER, s3_bucket=None, s3_key=None):
+        super().__init__(server_name=server_name, version=version, module_id_map=module_id_map)
         """Given an avro document, draw all frame_annotations
         
         Args:
@@ -41,41 +44,66 @@ class FrameDrawer(CVModule):
         """
         if avro_api:
             self.avro_api=avro_api
-        if self.avro_api==None:
-            log.error('No avro_api')
-            
-        if med_ret:
-            self.med_ret=med_ret
+            if med_ret:
+                self.med_ret=med_ret
+            else:
+                self.med_ret = MediaRetriever(self.avro_api.get_url())            
         else:
-            self.med_ret = MediaRetriever(self.avro_api.get_url())            
-        self.w, self.h = self.med_ret.get_w_h()
-        media_id = os.path.basename(self.avro_api.get_url()).rsplit(".", 1)[0]
-        self.media_id = "".join([e for e in media_id if e.isalnum() or e in ["/", "."]])
-        
-
+            self.avro_api=None
+            self.med_ret=None      
+       
+       
         if s3_bucket and not s3_key:
             raise ValueError("s3 bucket defined but s3 key not defined")
         if s3_key and not s3_bucket:
             raise ValueError("s3 key defined but s3 bucket not defined")
         if not pushing_folder and not s3_key:
             raise ValueError("pushing_folder and s3 key not defined, we cannot set where to dump.")
-        self.pushing_folder = pushing_folder
+        if pushing_folder:
+            self.pushing_folder = pushing_folder
+        else:
+            self.pushing_folder = DEFAULT_DUMP_FOLDER
+            
         self.s3_bucket = s3_bucket
         self.s3_key = s3_key
+        
+    def process(self,message=None, dump_video=True, dump_images=False):
+        if message:
+            log.info("Setting message")
+            self.set_message(message)
+            log.info("Processing message")
+        self.last_tstamp = 0.0
+        if message:
+            self.avro_api=self.request_api.get_avro_api()
+            self.med_ret=self.request_api.get_media_api()
+
+        self.w, self.h = self.med_ret.get_w_h()
+        media_id = os.path.basename(self.avro_api.get_url()).rsplit(".", 1)[0]
+        self.media_id = "".join([e for e in media_id if e.isalnum() or e in ["/", "."]])
         self.content_type_map={}
         
-    def process(self, dump_video=False, dump_images=False):
-        self.dump_video=dump_video
-        self.dump_images=dump_images
-        if dump_video==False  and dump_images==False:
-            log.warning("You may want to dump something.")
+        #if there is no flag in the request of not request_api we'll get None.
+        self.dump_video=None
+        self.dump_images=None
+        try:
+            self.dump_video=self.request_api.get("dump_video")
+            self.dump_images=self.request_api.get("dump_images")
+        except:
+            pass
+        if self.dump_video==None:
+            self.dump_video=dump_video
+        if self.dump_images==None:
+            self.dump_images=dump_images
+        if self.dump_video==False  and self.dump_images==False:
+            log.warning("You might want to dump something.")
             return
                        
         dump_folder= self.pushing_folder + '/' + self.media_id +'/'
+        self.dumping_folder_url=dump_folder
         if dump_folder:
             if not os.path.exists(dump_folder):
                 os.makedirs(dump_folder)        
-        if dump_video:
+        if self.dump_video:
             filename = dump_folder + '/video.mp4'            
             fps =1
             frameSize=self.med_ret.shape
@@ -106,6 +134,7 @@ class FrameDrawer(CVModule):
             exit(1)          
         
         for i, (img, tstamp) in enumerate(frames_iterator):
+            self.last_tstamp = tstamp
             if img is None:
                 log.warning("Invalid frame")
                 continue
@@ -128,18 +157,18 @@ class FrameDrawer(CVModule):
                     img = cv2.putText(img, prop, (p0[0]+3, p1[1]-3+i*25), face, 1.0, rand_color, thickness)
             #Include the timestamp
             img = cv2.putText(img, str(tstamp), (20, 20), face, scale, [255,255,255], thickness)
-            if dump_video:
+            if self.dump_video:
                 #we add the frame
                 log.debug("Adding frame")
                 vid.write(img)
-            if dump_images:
+            if self.dump_images:
                 #we dump the frame
                 outfn = "{}/{}.jpg".format(dump_folder, tstamp)
                 log.debug("Writing to file: {}".format(outfn))
                 cv2.imwrite(outfn, img)
                 self.content_type_map[os.path.basename(outfn)]='image/jpeg'
         
-        if  dump_video:            
+        if  self.dump_video:            
             vid.release()
         if self.s3_bucket:
             self.upload_files(dump_folder)
@@ -153,7 +182,7 @@ class FrameDrawer(CVModule):
         bucket = s3.Bucket(self.s3_bucket)
         key_root=self.s3_key + '/' +self.media_id + '/'
         #https://<bucket-name>.s3.amazonaws.com/<key>
-        self.dumping_folder_url='https://'+self.s3_bucket+'.s3.amazonaws.com/'+ key_root
+        self.dumping_folder_url='https://s3.amazonaws.com/'+self.s3_bucket+'/'+ key_root
 
 
         for subdir, dirs, files in os.walk(path):
@@ -162,7 +191,7 @@ class FrameDrawer(CVModule):
                 with open(full_path, 'rb') as data:
                     rel_path=os.path.basename(full_path)
                     key=key_root+ rel_path
-                    log.info('Pushing ' + full_path + ' to ' + key)
+                    log.info('Pushing ' + full_path + ' to ' + self.dumping_folder_url)
                     try:
                         content_type=self.content_type_map[os.path.basename(full_path)]
                     except:
@@ -186,7 +215,6 @@ class FrameDrawer(CVModule):
         self.avro_api.set_url(self.request_api.get_url())
         self.avro_api.set_url_original(self.request_api.get_url())
         self.avro_api.set_dims(*self.request_api.media_api.get_w_h())
-        url = self._s3_url_format.format(bucket=self._s3_bucket, s3_key=self.contents_file_key)
         module_id=0
         if self.module_id_map:
             module_id=self.module_id_map.get(self.name, 0)
@@ -195,7 +223,8 @@ class FrameDrawer(CVModule):
             p1 = create_prop(server=self.name, ver=self.version, value=self.dumping_folder_url, property_type="dumped_images", footprint_id=fp["id"], property_id=1,  module_id=module_id)
             p.append(p1)
         if self.dump_video:
-            dumped_video_url= self.dumping_folder_url+ '/video.mp4'            
+            dumped_video_url= self.dumping_folder_url+ '/video.mp4'
+            dumped_video_url=dumped_video_url.replace('//','/' )
             p2 = create_prop(server=self.name, ver=self.version, value=dumped_video_url, property_type="dumped_video", footprint_id=fp["id"], property_id=2,  module_id=module_id)
             p.append(p2)
         track = create_video_ann(t1=0.0, t2=self.last_tstamp, props=p)
