65b918b0ba90ccb9ad2d796b088cd02864b34d89
Matthew Greenberg <mgreenberg@gumgum.com>
2019-04-02 15:03:25 -0700

    removing deprecation warning

--- multivitamin/media/__init__.py
+++ multivitamin/media/__init__.py
@@ -1,12 +1,11 @@
-import glog as log
-import inspect
-media_retriever_deprecation = [
-    "from multivitamin.media import MediaRetriever",
-    "from multivitamin.media.media_api import MediaRetriever"
-    ]
-if inspect.stack()[-1].code_context in media_retriever_deprecation:
-    log.warning("Importing MediaRetriever will be deprecated. Please import "
-                "get_media_retriever from multivitamin.media")
+# import glog as log
+# import inspect
+# media_retriever_deprecation = [
+#     "from multivitamin.media.media_api import MediaRetriever"
+#     ]
+# if inspect.stack()[-1].code_context in media_retriever_deprecation:
+#     log.warning("Importing MediaRetriever will be deprecated. Please import "
+#                 "get_media_retriever from multivitamin.media")
 
 from .opencv_media_retriever import OpenCVMediaRetriever
 from .pims_media_retriever import PIMSMediaRetriever
