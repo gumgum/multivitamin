c667c775ec2de6a4b3b1d21c0cf2088c074fe033
Javier Molina <javier@gumgum.com>
2019-05-28 15:04:11 -0700

    Cleaning traces

--- multivitamin/module/module.py
+++ multivitamin/module/module.py
@@ -79,7 +79,7 @@ class Module(ABC):
         Args:
             responses list[Response]: responses
         """  
-        log.info("PROCESS***************")             
+        log.debug("PROCESS***************")             
         assert isinstance(responses, list)                
         for r in responses:                             
             r.tstamps_processed = []
@@ -122,24 +122,24 @@ class Module(ABC):
         Returns:
             list[Response]: output responses
         """
-        log.info("We clean self.responses")
+        log.debug("We clean self.responses")
         #we clean self.responses
         self.responses = []
-        log.info("We check timeouts")
+        log.debug("We check timeouts")
         #we mark as processed, with a timeout code, the responses from self.responses_to_be_processed that have been there for too long. 
         for r in self.responses_to_be_processed:
             r.check_timeout()
         #we move from to_be_processed to processed the responses already processed
-        log.info("We move the already processed responses from self.to_be_processed to self.responses")
-        log.info("len(self.responses_to_be_processed): " + str(len(self.responses_to_be_processed)))
-        log.info("len(self.responses): " + str(len(self.responses)))
+        log.debug("We move the already processed responses from self.to_be_processed to self.responses")
+        log.debug("len(self.responses_to_be_processed): " + str(len(self.responses_to_be_processed)))
+        log.debug("len(self.responses): " + str(len(self.responses)))
         for r in self.responses_to_be_processed:
             if r.is_already_processed():#if de the children doesn't activate the request's FSM, this will always returns true
                 self.responses.append(r)
         #we remove from to_be_processed the responses already processed
         self.responses_to_be_processed = [x for x in self.responses_to_be_processed if not x.is_already_processed()]
-        log.info("len(self.responses_to_be_processed): " + str(len(self.responses_to_be_processed)))
-        log.info("len(self.responses): " + str(len(self.responses)))        
+        log.debug("len(self.responses_to_be_processed): " + str(len(self.responses_to_be_processed)))
+        log.debug("len(self.responses): " + str(len(self.responses)))        
         #we update the responses
         for r in self.responses:                          
             self.update_and_return_response(response=r)
