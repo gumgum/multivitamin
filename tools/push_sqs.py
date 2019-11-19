5a6ea49d4db734a988cddd8cd2d491086a6ad56b
Matthew Greenberg <mgreenberg@gumgum.com>
2019-11-19 12:04:29 -0800

    fixing git ignore

--- tools/push_sqs.py
+++ tools/push_sqs.py
@@ -1,4 +1,4 @@
-import sys, os
+import os
 import argparse
 from glob import glob
 import boto3
@@ -9,15 +9,8 @@ a.add_argument("--queue_name")
 a.add_argument("--requests_dir")
 args = a.parse_args()
 
-#reqs = []
-#with open(args.request_txt) as rf:
-#    reqs = rf.readlines()
-
-#reqs = [r.strip() for r in reqs]
 queue = sqs.get_queue_by_name(QueueName=args.queue_name)
-#for req in reqs:
 for req in glob(os.path.join(args.requests_dir, "*")):
     with open(req) as rf:
         resp = queue.send_message(MessageBody=rf.read().strip())
         print(resp)
-    
